afdb1de950f911e4572306180c01b73e491056b8
André Mendes <develmendes@gmail.com>
2022-06-21 18:45:00 -0300

    finalizando enviando os dados para o localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -11,13 +11,19 @@ export default createStore({
       state.products = products;
     },
 
+    loadBag(state, products) {
+      state.productsInBag = products;
+    },
+
     addToBag(state, product) {
       state.productsInBag.push(product);
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag))
     },
 
     removeFromBag(state, productId) {
       const updatedBag = state.productsInBag.filter(item => productId != item.id);
       state.productsInBag = updatedBag;
+      localStorage.setItem("productsInBag", JSON.stringify(state.productsInBag))
     },
   },
   actions: {
@@ -27,6 +33,12 @@ export default createStore({
       .then(response => {
         commit('loadProducts', response.data);
       })
+    },
+
+    loadBag({commit}) {
+        if (localStorage.getItem("productsInBag")){ 
+          commit('loadBag', JSON.parse(localStorage.getItem("productsInBag")));
+        }
     },
     
     addToBag({ commit }, product) {
